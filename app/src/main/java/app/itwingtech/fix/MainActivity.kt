3bb491d67c6de21a44dc6c4dfb8f3c15a83e2985
Tanzeelzahrakhan <tanzeelzahra72@gmail.com>
2024-09-17 11:18:09 +0500

    shake library

---
+++ app/src/main/java/app/itwingtech/fix/MainActivity.kt
@@ -0,0 +1,36 @@
+package app.itwingtech.fix
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import app.itwingtech.fix.databinding.ActivityMainBinding
+import app.itwingtech.shakelibrary.ShakeLib
+import app.itwingtech.shakelibrary.ShakeLibraryView
+
+class MainActivity : AppCompatActivity() {
+
+private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        ShakeLib.shake(binding.tvShake,10000,10f)
+
+
+
+
+    }
+}
